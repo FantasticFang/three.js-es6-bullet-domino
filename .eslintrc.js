9f2e74cbe84abcf5d9a8f8115fbe883956111f9a
whugis98_Fang <whugis98_Fang@tencent.com>
2018-09-16 19:11:14 +0800

    ok提交

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    extends: ['eslint-config-alloy'],
+    parserOptions: {
+        // @see https://github.com/eslint/eslint/issues/10307
+        // override 2017
+        'ecmaVersion': 9,
+        'sourceType': 'module'
+    },
+    globals: {
+        // 这里填入你的项目需要的全局变量
+        // 这里值为 false 表示这个全局变量不允许被重新赋值，比如：
+        //
+        // jQuery: false,
+        // $: false
+
+        // 这里定义好在polyfills里全局符号，防止误报
+        'joinPaths': false,
+        'searchFileNameFromURL': false,
+        'formatPathFromURL': false,
+        'IsFloat32Array': false,
+        'isArrayBuffer': false,
+        'clamp01': false,
+        'msgpack': false
+    },
+    rules: {
+        // 这里填入你的项目需要的个性化配置，比如：
+        //
+        // // @fixable 一个缩进必须用两个空格替代
+        // 'indent': [
+        //     'error',
+        //     2,
+        //     {
+        //         SwitchCase: 1,
+        //         flatTernaryExpressions: true
+        //     }
+        // ]
+        'no-empty-function': 'off'
+    }
+};
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,36 @@
 const path = require('path');
 
+// 向上一级得到项目的根目录
+function resolve(folder) {
+    return path.resolve(__dirname, '..', folder);
+}
+// ------------------------------------------------------------------------------------------------
+
 module.exports = {
-  entry: './src/test.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '..','dist')
-  }
+    mode: 'development',
+
+    entry: './src/test.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '..', 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: resolve('node_modules'),
+                include: resolve('src'),
+                use: {
+                    loader: 'eslint-loader',
+                    options: {
+                        baseConfig: false
+                        // configFile: resolve('.eslintrc.js')
+                    }
+                }
+            }
+        ]
+    }
 };
