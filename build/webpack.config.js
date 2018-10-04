1ffd809528b806ba4ff322d21f9611455e81ca36
whugis98_Fang <whugis98_Fang@tencent.com>
2018-10-04 16:37:03 +0800

    webpack加上babel的一些配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+// const webpack = require('webpack');
+const env = process.env.npm_package_config_webpack_env;
+// const hot = process.env.npm_package_config_webpack_env_devServer;
+const libraryName = 'project3d';
 
 // 向上一级得到项目的根目录
 function resolve(folder) {
@@ -6,14 +10,32 @@ function resolve(folder) {
 }
 // ------------------------------------------------------------------------------------------------
 
+let outputFile = '';
+let plugins = [];
+let minimize = false;
+
+if (env === 'release') {
+    minimize = true;
+    outputFile = libraryName + '.min.js';
+} else {
+    // this will be included automatically when enable inline property of devServer
+    // plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    outputFile = libraryName + '.js';
+}
+
 module.exports = {
     mode: 'development',
-
     entry: './src/test.js',
 
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '..', 'dist')
+        filename: outputFile,
+        path: path.resolve(__dirname, '..', 'dist'),
+        library: libraryName
+    },
+
+    optimization: {
+        minimize: minimize
     },
 
     module: {
@@ -30,7 +52,41 @@ module.exports = {
                         // configFile: resolve('.eslintrc.js')
                     }
                 }
+            },
+            {
+                test: /\.js$/,
+                exclude: [
+                    resolve('node_modules'),
+                    resolve('worker')
+                ],
+                include: [
+                    resolve('src'),
+                    resolve('libs')
+                ],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            // ['transform-object-rest-spread', { 'useBuiltIns': true }],
+                            ['transform-runtime', { 'polyfill': false, 'regenerator': false }],
+                            ['transform-es2015-template-literals']
+                        ],
+                        presets: [
+                            ['env', {
+                                targets: {
+                                    browsers: ['last 5 versions', 'chrome >= 52', 'FireFox >= 44', 'Safari >= 7', 'Explorer 11', 'last 4 Edge versions']
+                                },
+                                modules: 'umd',
+                                loose: false,
+                                // https://www.jianshu.com/p/0dc3bddb6da8
+                                useBuiltIns: 'false' // "usage" | "entry" | false
+                            }]
+                        ]
+                    }
+                }
             }
         ]
-    }
+    },
+
+    plugins: plugins
 };
